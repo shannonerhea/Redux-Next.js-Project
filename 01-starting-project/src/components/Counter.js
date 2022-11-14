50e8972cee90db11830c8a324ed9c3d1a813e012
shannone <shannone426@gmail.com>
2022-11-14 11:20:19 -0500

    redux

--- 01-starting-project/src/components/Counter.js
+++ 01-starting-project/src/components/Counter.js
@@ -11,6 +11,10 @@ const counter = useSelector(state => state.counter);
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
