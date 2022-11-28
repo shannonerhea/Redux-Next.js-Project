787f811ba04661673cb132c2b8d9c59a27c69615
shannone <shannone426@gmail.com>
2022-11-28 10:58:28 -0500

    redux toolkit

--- 01-starting-project/src/store/index.js
+++ 01-starting-project/src/store/index.js
@@ -1,33 +1,52 @@
 import { createStore } from "redux";
-const initialState = { counter: 0, showCounte: true,}
+import { createSlice } from "@reduxjs/toolkit";
 
-const counterReducer = (state = initialState , action) => {
+const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
       showCounter: state.showCounter,
     };
   }
-  if (action.type === 'increase') {
+  if (action.type === "increase") {
     return {
-        counter: state.counter + action.amount,
-        showCounter: state.showCounter,
-    }
-  };
-  if (action.type === "decrement") {
-    return {
-        counter: state.counter - 1,
-        showCounter: state.showCounter,
-       
-
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
-  if (action.type === 'toggle') {
-return {
-    showCounter: !state.showCounter,
-    counter: state.counter,
-}
-  };
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
+  }
   return state;
 };
 
